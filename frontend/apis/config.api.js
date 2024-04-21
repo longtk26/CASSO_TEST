5cd205dba0e80f39e3cbf58586dcc4af24ba1ac7
longtk26 <longtk26@gmail.com>
2024-04-21 13:46:50 +0700

    production: fe & be to production

--- frontend/apis/config.api.js
+++ frontend/apis/config.api.js
@@ -1,3 +1,3 @@
 export const SERVER_URL = "https://casso-test.onrender.com/v1/api";
-export const SUCCESS_URL = "http://localhost:3000/payment-success";
-export const CANCEL_URL = "http://localhost:3000/payment-cancel";
+export const SUCCESS_URL = "https://casso-ebooks.vercel.app/payment-success";
+export const CANCEL_URL = "https://casso-ebooks.vercel.app/payment-cancel";
